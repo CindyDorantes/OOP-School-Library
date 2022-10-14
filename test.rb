03f2b656459be646d05c8bd8c14183651498e23b
CindyDorantes <cindym.dorantes@gmail.com>
2022-10-14 14:56:38 -0500

    fix linter errors

--- test.rb
+++ test.rb
@@ -3,21 +3,25 @@ require_relative './classroom'
 require_relative './book'
 require_relative './rental'
 
-tiago = Student.new(33, 'Tiago')
-cindy = Student.new(33, 'Cindy')
+# tiago = Student.new(33, 'Tiago')
+# cindy = Student.new(33, 'Cindy')
 
-class_math = Classroom.new('math')
-class_math.add_student(tiago)
-class_math.add_student(cindy)
+# class_math = Classroom.new('math')
+# class_math.add_student(tiago)
+# class_math.add_student(cindy)
 
-andy = Person.new(33, 'Andy')
+# andy = Person.new(33, 'Andy')
+# cindy = Person.new(33, 'Cindy')
 
-book1 = Book.new('La iliada', 'Homero')
-book2 = Book.new('cuentos', 'Asimov')
+# book1 = Book.new('La iliada', 'Homero')
+# book2 = Book.new('cuentos', 'Asimov')
 
-rental01 = Rental.new('2022-10-12', book1, andy)
-rental02 = Rental.new('2022-10-12', book2, andy)
-puts rental01
-puts rental02
+# rental01 = Rental.new('2022-10-12', book1, andy)
+# rental02 = Rental.new('2022-10-12', book2, andy)
+# rental03 = Rental.new('2022-08-10', book1, cindy)
+# rental04 = Rental.new('2022-08-12', book2, cindy)
 
-# puts andy.rentals.map( { |rental| rental.book.title } )
+# puts andy.rentals.map { |rental| rental.book.title }
+# puts cindy.rentals.map { |rental| rental.book.title }
+# puts book1.rentals.map { |rental| rental.person.name }
+# puts book1.rentals.map { |rental| rental.person.name }
