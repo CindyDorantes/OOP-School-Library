b4f1ec88a2894f1a25339e2df47d6c60557ec383
CindyDorantes <cindym.dorantes@gmail.com>
2022-10-14 19:36:34 -0500

    create class App

---
+++ app.rb
@@ -0,0 +1,12 @@
+require_relative './student'
+
+class App
+
+  def initialize
+    @books = []
+    @people = []
+    @rentals = []
+  end
+
+
+end
--- test.rb
+++ test.rb
@@ -1,27 +1,4 @@
-require_relative './student'
-require_relative './classroom'
-require_relative './book'
-require_relative './rental'
+require_relative './app'
 
-# tiago = Student.new(33, 'Tiago')
-# cindy = Student.new(33, 'Cindy')
-
-# class_math = Classroom.new('math')
-# class_math.add_student(tiago)
-# class_math.add_student(cindy)
-
-# andy = Person.new(33, 'Andy')
-# cindy = Person.new(33, 'Cindy')
-
-# book1 = Book.new('La iliada', 'Homero')
-# book2 = Book.new('cuentos', 'Asimov')
-
-# rental01 = Rental.new('2022-10-12', book1, andy)
-# rental02 = Rental.new('2022-10-12', book2, andy)
-# rental03 = Rental.new('2022-08-10', book1, cindy)
-# rental04 = Rental.new('2022-08-12', book2, cindy)
-
-# puts andy.rentals.map { |rental| rental.book.title }
-# puts cindy.rentals.map { |rental| rental.book.title }
-# puts book1.rentals.map { |rental| rental.person.name }
-# puts book1.rentals.map { |rental| rental.person.name }
+new_app = App.new
+puts new_app
