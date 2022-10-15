4d61b9253b5f06784e1352d3a16473840c085206
CindyDorantes <cindym.dorantes@gmail.com>
2022-10-14 20:06:13 -0500

    create methods to create Student and Teacher

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require_relative './student'
 
 class App
+  attr_accessor :books, :people, :rentals
 
   def initialize
     @books = []
@@ -8,5 +9,26 @@ class App
     @rentals = []
   end
 
+  def create_student
+    print 'Age:'
+    age = gets.chomp
+    print 'Name:'
+    name = gets.chomp
+    print 'Has parent permission?[Y/N]:'
+    permission = gets.chomp
+    @people << Student.new(age, name, parent_permission: permission.upcase == 'Y')
+    puts 'Student created successfully'
+  end
+
+  def create_teacher
+    print 'Age:'
+    age = gets.chomp
+    print 'Name:'
+    name = gets.chomp
+    print 'Specialization:'
+    specialization = gets.chomp
+    @people << Teacher.new(specialization, age, name)
+    puts 'Teacher created successfully'
+  end
 
 end
--- test.rb
+++ test.rb
@@ -1,4 +1,7 @@
 require_relative './app'
+require_relative './student'
 
 new_app = App.new
 puts new_app
+
+new_app.create_student
