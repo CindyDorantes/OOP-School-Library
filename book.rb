c8596ed2fa79d07483b54632288343e406365eb3
renercrows <ungredda95@gmail.com>
2022-10-17 19:00:48 -0400

    Remove Books Refactor

--- book.rb
+++ book.rb
@@ -7,32 +7,3 @@ class Book
     @rentals = []
   end
 end
-
-class Book
-  attr_accessor :title, :author, :rentals
-
-  def initialize(title, author)
-    @title = title
-    @author = author
-    @rentals = []
-  end
-end
-
-def create_book(books)
-  print 'Title: '
-  title = gets.chomp
-  print 'Author: '
-  author = gets.chomp
-  books << Book.new(title, author)
-  puts 'Book created successfully'
-end
-
-def list_books(books)
-  if books.length.zero?
-    puts 'There are not books registered in the system'
-  else
-    books.each_with_index do |book, idx|
-      puts "#{idx}) [#{book.class}] Title: #{book.title}, Author: #{book.author}"
-    end
-  end
-end
